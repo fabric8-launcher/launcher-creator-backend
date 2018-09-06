4811bcb6c792f47a8f28bcae2c76ba0bd91891bf
Tako Schotanus <tako@ceylon-lang.org>
2018-09-06 14:29:37 +0200

    feat(generators): Added `rest-vertx` Generator

---
+++ generators/rest-vertx/files/src/test/java/io/openshift/booster/OpenShiftIT.java
@@ -0,0 +1,43 @@
+package io.openshift.booster;
+
+import com.jayway.restassured.RestAssured;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.jayway.restassured.RestAssured.get;
+import static io.openshift.booster.HttpApplication.template;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@RunWith(Arquillian.class)
+public class OpenShiftIT {
+
+    private final String applicationName = System.getProperty("app.name", "http-vertx");
+
+    @RouteURL("${app.name}")
+    @AwaitRoute
+    private URL route;
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = route.toString();
+    }
+
+    @Test
+    public void testThatWeAreReady() throws Exception {
+        // Check that the route is served.
+        get().then().statusCode(equalTo(200));
+        get("/api/greeting").then().statusCode(equalTo(200));
+    }
+
+    @Test
+    public void testThatWeServeAsExpected() throws MalformedURLException {
+        get("/api/greeting").then().body("content", equalTo(String.format(template, "World")));
+        get("/api/greeting?name=vert.x").then().body("content", equalTo(String.format(template, "vert.x")));
+    }
+}
---
+++ generators/rest-vertx/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs-extra');
+const path = require('path');
+const newApp = require('@core/resources').newApp;
+const moduleDir = require('@core/utils').moduleDir;
+const getGeneratorModule = require("launcher-creator-catalog").getGeneratorModule;
+
+exports.apply = function(targetDir, props={}) {
+    // First copy the files from the base Vert.x platform module
+    // and then copy our own over that
+    return getGeneratorModule("platform-vertx").apply(targetDir, props)
+        .then(() => fs.copy(path.join(moduleDir(module), "files"), targetDir));
+    // TODO Don't just blindly copy all files, we need to _patch_ some of
+    // them instead (eg. pom.xml and arquillian.xml and Java code)
+}
+
+exports.generate = function(resources, targetDir, props = {}) {
+    // Just call into the base Vert.x platform module, we don't
+    // need to add anything ourselves
+    return getGeneratorModule("platform-vertx").generate(resources, targetDir, props);
+};
+
+exports.info = function () {
+    return require("./info.json");
+};
