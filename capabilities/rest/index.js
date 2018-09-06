caf9da83a3ac0044022f2908867b18e6f0d20ea7
Tako Schotanus <tako@ceylon-lang.org>
2018-09-06 14:35:07 +0200

    feat(capabilities): Added `rest` Capability
    Only support Vert.x for now

---
+++ capabilities/rest/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const getGeneratorModule = require("launcher-creator-catalog").getGeneratorModule;
+
+// Returns the corresponding runtime generator depending on the given runtime type
+function runtimeByType(type) {
+    if (type === "vertx") {
+        return getGeneratorModule("rest-vertx");
+    } else {
+        throw `Unsupported runtime type: ${type}`;
+    }
+}
+
+exports.apply = function(capName, targetDir, props) {
+    const rtprops = {};
+    return runtimeByType(props.runtime).apply(targetDir, rtprops);
+};
+
+exports.generate = function(capName, resources, targetDir, props) {
+    const rtprops = {};
+    return runtimeByType(props.runtime).generate(res, targetDir, rtprops);
+};
+
+exports.info = function() {
+    return require("./info.json");
+};
