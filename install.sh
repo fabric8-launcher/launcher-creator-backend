28d89f564807797949a210f487d034fcba77a796
Tako Schotanus <tako@ceylon-lang.org>
2018-09-14 00:00:41 +0200

    refactor(scripts): Install is quiet and tests get run

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-[ ! -f "lib/core/maven/maven-model-helper.jar" ] && curl https://oss.sonatype.org/content/repositories/releases/io/fabric8/maven-model-helper/5/maven-model-helper-5-uber.jar -o lib/core/maven/maven-model-helper.jar
+[ ! -f "lib/core/maven/maven-model-helper.jar" ] && curl -sSL https://oss.sonatype.org/content/repositories/releases/io/fabric8/maven-model-helper/5/maven-model-helper-5-uber.jar -o lib/core/maven/maven-model-helper.jar
 exit 0
