fa58105e0f040de79bfbf3cf4e6f0fb61b897909
Tako Schotanus <tako@ceylon-lang.org>
2018-09-19 00:20:36 +0200

    feat(scripts): Made `install.sh` react to the `--force` option

--- install.sh
+++ install.sh
@@ -6,5 +6,10 @@ ARTIFACT_URL="https://oss.sonatype.org/content/repositories/releases/io/fabric8/
 TARGET_DIR="lib/core/maven"
 TARGET="${TARGET_DIR}/maven-model-helper.jar"
 
-[ ! -f "${TARGET}" ] && mkdir -p "${TARGET_DIR}" && curl -sSL "${ARTIFACT_URL}" -o "${TARGET}"
+FORCE=0
+if [[ ${npm_config_force} = "true" || ${npm_config_argv} = *"--force"* ]]; then
+    FORCE=1
+fi
+
+[[ ! -f "${TARGET}" || ${FORCE} -eq 1 ]] && mkdir -p "${TARGET_DIR}" && curl -sSL "${ARTIFACT_URL}" -o "${TARGET}"
 exit 0
