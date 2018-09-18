220c540eace954267a338361ef72f8ec2d58644d
George Gastaldi <gegastaldi@gmail.com>
2018-09-18 11:50:07 -0300

    feat: use sourceDominant=true while merging pom.xml files
    This guarantees that the <properties> set in the source pom.xml are replaced in the target pom.xml

--- lib/core/maven/index.ts
+++ lib/core/maven/index.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 const jar = join(__dirname, 'maven-model-helper.jar');
 
 export function mergePoms(targetPath, sourcePath) {
-    const proc = spawn('java', ['-jar', jar, targetPath, sourcePath])
+    const proc = spawn('java', ['-DsourceDominant=true','-jar', jar, targetPath, sourcePath])
         .catch((error) => {
             console.error(`Spawn error: ${error}`);
             throw error;
