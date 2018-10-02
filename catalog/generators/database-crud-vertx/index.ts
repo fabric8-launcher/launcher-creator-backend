c1f1c1a33a8128bb775e840d24c467227e5c5b0c
Tako Schotanus <tako@ceylon-lang.org>
2018-10-02 13:20:58 +0200

    feat(deploy): Generated project now has a deploy/push script
    This get specifically generated for the application so the user
    doesn't need to know about specific arguments to pass.
    
    Fixes #18

--- catalog/generators/database-crud-vertx/index.ts
+++ catalog/generators/database-crud-vertx/index.ts
@@ -14,10 +14,14 @@ export function apply(applyGenerator, resources, targetDir, props: any = {}) {
         'artifactId': props.artifactId,
         'version': props.version,
     };
+    const tprops = {
+        ...props,
+        'serviceName': props.application + '-vertx'
+    };
     return applyGenerator('platform-vertx', resources, targetDir, pprops)
         .then(() => copy(join(__dirname, 'files'), targetDir))
         .then(() => mergePoms(join(targetDir, 'pom.xml'), join(__dirname, 'merge', `pom.${props.databaseType}.xml`)))
-        .then(() => transformFiles(join(targetDir, 'src/**/*.java'), cases(props)))
+        .then(() => transformFiles([join(targetDir, 'src/**/*.java'), join(targetDir, 'gap')], cases(tprops)))
         .then(() => resources);
     // TODO Don't just blindly copy all files, we need to _patch_ some of
     // them instead (eg. pom.xml and arquillian.xml and Java code)
