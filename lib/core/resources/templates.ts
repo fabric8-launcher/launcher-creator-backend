684b51e18f5ce24ac5d0f59998979279823cf547
Tako Schotanus <tako@ceylon-lang.org>
2018-10-02 19:07:15 +0200

    feat(resources): Builder templates are now OS templates too
    The builder templates now support the Launcher variables
    `SOURCE_REPOSITORY_URL` and `GITHUB_WEBHOOK_SECRET`.
    
    Fixes #25 and partly implements #26

--- lib/core/resources/templates.ts
+++ lib/core/resources/templates.ts
@@ -9,6 +9,36 @@ const dummyName = 'placeholder-app-name-730040e0c873453f877c10cd07912d1f';
 const dummyLabel = 'placeholder-app-label-d46881878f594a2dadfd963843452aab';
 const dummyNameRe = new RegExp(dummyName, 'g');
 const dummyLabelRe = new RegExp(dummyLabel, 'g');
+const dummyGitUrl = 'https://github.com/dummy_org/dummy_repo';
+
+const sourceRepoUrlParam = {
+    'name': 'SOURCE_REPOSITORY_URL',
+    'description': 'The source URL for the application',
+    'displayName': 'Source URL',
+    'value': dummyGitUrl,
+    'required': true
+};
+
+const githubWebhookSecretParam = {
+    'name': 'GITHUB_WEBHOOK_SECRET',
+    'description': 'The secret used to configure a GitHub webhook',
+    'displayName': 'GitHub Webhook Secret',
+    'required': true,
+    'from': '[a-zA-Z0-9]{40}',
+    'generate': 'expression'
+};
+
+const buildTriggers = [{
+    'type': 'GitHub',
+    'github': {
+        'secret': '${GITHUB_WEBHOOK_SECRET}'
+    }
+}, {
+    'type': 'ConfigChange'
+}, {
+    'type': 'ImageChange',
+    'imageChange': {}
+}];
 
 function normalizeImageName(img: string) {
     return img.replace(/\//g, '_');
@@ -21,13 +51,21 @@ function templateFileName(img: string) {
 export function generate() {
     const images = require('./images.json');
     images.forEach(({image, isBuilder}) => {
-        const srcUri = isBuilder ? 'https://github.com/dummy/dummy' : null;
-        return newApp(dummyName, dummyLabel, image, srcUri,{})
+        const srcUri = isBuilder ? dummyGitUrl : null;
+        return newApp(dummyName, dummyLabel, image, srcUri, {})
             .then((res) => {
                 if (isBuilder) {
+                    // Turn the resources into a template and add parameters
+                    res
+                        .addParam(sourceRepoUrlParam)
+                        .addParam(githubWebhookSecretParam);
                     // Make sure incremental builds are enabled
                     const bc = res.buildConfig(dummyName);
                     _.set(bc, 'spec.strategy.sourceStrategy.incremental', true);
+                    // Set the Git repo URL to use the template parameter
+                    _.set(bc, 'spec.source.git.uri', '${SOURCE_REPOSITORY_URL}');
+                    // Set the GitHub webhook trigger to use the template parameter
+                    _.set(bc, 'spec.triggers', buildTriggers);
                 }
                 // Write the resources to a file
                 const name = templateFileName(image);
