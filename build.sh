2e5ebdae84c83d13607d1558f9580628f249b757
Tako Schotanus <tako@ceylon-lang.org>
2018-10-03 16:51:26 +0200

    fix(build): Build script now properly copies all files to `dist`

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 node node_modules/typescript/bin/tsc --project .
 
-find lib/ \( -type f -name '*.json' -o -name '*.jar' \) -exec sh -c 'mkdir -p dist/$(dirname {}) && cp {} dist/{}' \;
-find catalog/ \( -type f -name '*.json' -o -name '*.jar' \) -exec sh -c 'mkdir -p dist/$(dirname {}) && cp {} dist/{}' \;
+find lib/ -type f ! -name '*.ts' -exec sh -c 'mkdir -p "dist/$(dirname "{}")" && cp "{}" "dist/{}"' \;
+find catalog/ -type f ! -name '*.ts' -exec sh -c 'mkdir -p "dist/$(dirname "{}")" && cp "{}" "dist/{}"' \;
