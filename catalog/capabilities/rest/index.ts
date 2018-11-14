acce0b4ca96aa2f40abd3808c9863694e3c0e9ec
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-11-14 10:59:35 +0100

    fix: initial nodejs version

--- catalog/capabilities/rest/index.ts
+++ catalog/capabilities/rest/index.ts
@@ -2,11 +2,14 @@
 import { BaseCapability } from 'core/catalog';
 
 import RestVertx from 'generators/rest-vertx';
+import RestNodejs from 'generators/rest-nodejs';
 
 // Returns the corresponding runtime generator depending on the given runtime type
 function runtimeByType(type) {
     if (type === 'vertx') {
         return RestVertx;
+    } else if (type === 'nodejs') {
+        return RestNodejs;
     } else {
         throw new Error(`Unsupported runtime type: ${type}`);
     }
