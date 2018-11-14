3010da01b212e6fbc4d0870220e216d06c7485c0
George Gastaldi <gegastaldi@gmail.com>
2018-11-14 12:18:39 +0000

    chore: add openshift files
    Fixes #63

---
+++ cico_run_tests.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+BUILDER_IMAGE="launcher-creator-backend-builder"
+BUILDER_CONT="launcher-creator-backend-builder-container"
+DEPLOY_IMAGE="launcher-creator-backend-deploy"
+
+TARGET_DIR="dist"
+
+# Exit on error
+set -e
+
+if [ -z $CICO_LOCAL ]; then
+    [ -f jenkins-env ] && cat jenkins-env | grep -e PASS -e USER -e GIT -e DEVSHIFT > inherit-env
+    [ -f inherit-env ] && . inherit-env
+
+    # We need to disable selinux for now, XXX
+    /usr/sbin/setenforce 0
+
+    # Get all the deps in
+    yum -y install docker make git
+
+    # Get all the deps in
+    yum -y install docker make git
+    service docker start
+fi
+
+#CLEAN
+docker ps | grep -q ${BUILDER_CONT} && docker stop ${BUILDER_CONT}
+docker ps -a | grep -q ${BUILDER_CONT} && docker rm ${BUILDER_CONT}
+rm -rf ${TARGET_DIR}/
+
+#BUILD
+docker build -t ${BUILDER_IMAGE} -f Dockerfile.build .
+
+mkdir ${TARGET_DIR}/
+docker run --detach=true --name ${BUILDER_CONT} -t -v $(pwd)/${TARGET_DIR}:/${TARGET_DIR}:Z ${BUILDER_IMAGE} /bin/tail -f /dev/null #FIXME
+
+docker exec ${BUILDER_CONT} yarn test
+
+#BUILD DEPLOY IMAGE
+docker build -t ${DEPLOY_IMAGE} -f Dockerfile.deploy .
+
