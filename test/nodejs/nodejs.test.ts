97fc2dd2f400eabbd9449343031ff4925adc9da0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-11-15 13:39:34 +0100

    fix: added way to merge dependencies for package.json

---
+++ test/nodejs/nodejs.test.ts
@@ -0,0 +1,26 @@
+import * as test from 'tape';
+import { fileSync } from 'tmp';
+import { writeFileSync, readFileSync } from 'fs-extra';
+import { mergePackageJson } from 'core/nodejs';
+
+test('update parent dependencies', (t) => {
+  t.plan(1);
+
+  // Write target (original) file
+  const targetFile = fileSync();
+  writeFileSync(targetFile.name,
+    `{"name": "test", "dependencies": {"test": "1.0.0"}}`, 'utf8');
+
+  // Write source file
+  const sourceFile = fileSync();
+  writeFileSync(sourceFile.name,
+    `{"dependencies": {"super": "2.0.2"}}`, 'utf8');
+
+  mergePackageJson(targetFile.name, sourceFile.name)
+    .then(() => {
+      const result = readFileSync(targetFile.name, 'utf8');
+      const expected = '{\n  "name": "test",\n  "dependencies": {\n    "test": "1.0.0",\n    "super": "2.0.2"\n  }\n}';
+      t.is(result, expected);
+    });
+
+});
