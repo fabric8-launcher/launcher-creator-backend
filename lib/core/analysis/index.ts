65f5c50ab94dbbdf2b2bd7053f6dec95a80398e6
Tako Schotanus <tako@ceylon-lang.org>
2019-03-04 21:14:50 +0100

    chore(analysis): Re-added ENV vars and some cleanup

--- lib/core/analysis/index.ts
+++ lib/core/analysis/index.ts
@@ -1,5 +1,5 @@
 
-import {pathExists, readFile} from 'fs-extra';
+import { pathExists, readFile } from 'fs-extra';
 import { join } from 'path';
 import * as tmp from 'tmp-promise';
 import { spawn } from 'child-process-promise';
@@ -17,7 +17,7 @@ export async function determineBuilderImage(dir: string): Promise<BuilderImage> 
         throw new Error('Directory doesn\'t exist');
     }
     if (await pathExists(join(dir, 'pom.xml'))) {
-        if(isJavaee(await readFile(join(dir, 'pom.xml'), 'utf8')) ) {
+        if (isJavaee(await readFile(join(dir, 'pom.xml'), 'utf8')) ) {
             return builderById(BUILDER_JAVAEE);
         }
         return builderById(BUILDER_JAVA);
@@ -27,16 +27,23 @@ export async function determineBuilderImage(dir: string): Promise<BuilderImage> 
         return null;
     }
 }
-export function isJavaee(pom: string): Boolean {
+
+export function isJavaee(pom: string): boolean {
     return pom.indexOf('<packaging>war</packaging>') >= 0 && pom.indexOf('thorntail') < 0;
 }
-
 
 export async function determineBuilderImageFromGit(gitRepoUrl: string): Promise<BuilderImage> {
     // Create temp dir
     const td = await tmp.dir({ 'unsafeCleanup': true });
     // Shallow-clone the repository
-    await spawn('git', ['clone', gitRepoUrl, '--depth=1', td.path.toString()])
+    const env = {
+        'GIT_TERMINAL_PROMPT': '0',
+        // Work-around for problem in older Gits
+        // https://github.com/git/git/commit/92bcbb9b338dd27f0fd4245525093c4bce867f3d
+        'GIT_COMMITTER_NAME': 'dummy',
+        'GIT_COMMITTER_EMAIL': 'dummy'
+    };
+    await spawn('git', ['clone', gitRepoUrl, '--depth=1', td.path.toString()], { env })
         .catch((error) => {
             console.error(`Spawn error: ${error}`);
             throw error;
