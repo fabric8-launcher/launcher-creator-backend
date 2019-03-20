dfadcb481f179f0da0d299ff902af485a1b9c603
Omair Majid <omajid@redhat.com>
2019-03-20 14:43:26 -0400

    chore(generators): Use the correct compatiblity level for dotnet
    The compatibility level should be set to the latest version unless
    there's a real need for backwards compatiblity.

--- catalog/generators/platform-dotnet/files/Startup.cs
+++ catalog/generators/platform-dotnet/files/Startup.cs
@@ -30,7 +30,7 @@ namespace {{.dotnet.namespace}}
 
             services.AddHealthChecks();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
