5f8ebaffdb18836a3f1328cbb9a2d9499ff482e4
Tako Schotanus <tako@ceylon-lang.org>
2019-05-07 19:05:08 +0200

    feat(script): Platform update script now supports Vue

--- update-platform-generators.sh
+++ update-platform-generators.sh
@@ -30,9 +30,9 @@ EOT
     mv README.md.tmp README.md
     jq '.name = "{{.nodejs.name}}" | .version = "{{.nodejs.version}}"' package.json > package.json.tmp
     mv package.json.tmp package.json
-    find . \( -name "*.ts" -o -name "*.json" -o -name "*.html" \) -exec sed -i "s/$name/\{\{.nodejs.name\}\}/g" '{}' \;
+    find . \( -name "*.ts" -o -name "*.json" -o -name "*.html" -o -name "*.md" \) -exec sed -i "s/$name/\{\{.nodejs.name\}\}/g" '{}' \;
     popd
-    cp -a $tmp/my-angular-app/* catalog/generators/platform-angular/files/
+    cp -a $tmp/$name/* catalog/generators/platform-angular/files/
     rm -rf $tmp
 }
 
@@ -67,10 +67,46 @@ EOT
     jq '.name = "{{.nodejs.name}}" | .version = "{{.nodejs.version}}"' package.json > package.json.tmp
     mv package.json.tmp package.json
     popd
-    cp -a $tmp/my-react-app/* catalog/generators/platform-react/files/
+    cp -a $tmp/$name/* catalog/generators/platform-react/files/
     rm -rf $tmp
 }
 
+update_vuejs() {
+    local tmp=$(mktemp -d)
+    local name="my-vuejs-app"
+    pushd $tmp
+    npx @vue/cli create --default $name
+    cd $name
+    rm -rf node_modules package-lock.json yarn.lock
+    cat >>README.md.tmp <<'EOT'
+# platform-vuejs
+
+Created by the Cloud App Generator
+
+Now that the application has been generated it can be deployed in the currently active project on OpenShift by running:
+
+```
+$ ./gap deploy
+```
+
+Now the only thing that is left to do is push the project's code to OpenShift to be run. To push the sources and
+have the project be built on OpenShift you can do the following:
+
+```
+$ ./gap push
+```
+
+EOT
+    cat README.md >>README.md.tmp
+    mv README.md.tmp README.md
+    jq '.name = "{{.nodejs.name}}" | .version = "{{.nodejs.version}}"' package.json > package.json.tmp
+    mv package.json.tmp package.json
+    find . \( -name "*.ts" -o -name "*.json" -o -name "*.html" -o -name "*.md" \) -exec sed -i "s/$name/\{\{.nodejs.name\}\}/g" '{}' \;
+    popd
+    cp -a $tmp/$name/* catalog/generators/platform-vuejs/files/
+    rm -rf $tmp
+}
 update_angular
 update_react
+update_vuejs
 
